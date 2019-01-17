537ca280f517bb10abd87e5cb9b0e61725f5727f
simonmrog <simonmrog@gmail.com>
2019-01-16 19:31:19 -0500

    mountain slope calculator

--- mountain-slope.py
+++ mountain-slope.py
@@ -1,6 +1,7 @@
 # Program used for hiking, to calculate the slope of a mountain based
 # on data given by a gps program
 
+import math
 import click
 
 @click.group ()
@@ -10,16 +11,16 @@ def main ():
 
 # implementation for command line using click framework
 @main.command ()
-@click.argument ("x1")
+@click.argument ("r1")
 @click.argument ("y1")
-@click.argument ("x2")
+@click.argument ("r2")
 @click.argument ("y2")
-def c (x1, y1, x2, y2):
-    # x1, x2 in kms and y1, y2 in metres
-    """ Calculates the slope with parameters: x1, y1, x2, y2 """
-    delta_x = abs (float (x1) - float (x2)) * 1000 #delta_x in metres
+def c (r1, y1, r2, y2):
+    # r1, r2 in kms and y1, y2 in metres
+    """ Calculates the slope with parameters: r1, y1, r2, y2 """
+    delta_r = abs (float (r1) - float (r2)) * 1000 #delta_r in metres
     delta_y = abs (float (y1) - float (y2)) #delta_y is already in metres
-    m_percent = delta_y / delta_x * 100
+    m_percent = delta_y / math.sqrt (delta_r ** 2 - delta_y ** 2) * 100
     click.echo ("Slope: %.2f%%" % m_percent)
 
 
