db42e1d11f7efcbdfb46ae65b7d51e2dc70aeb6d
simonmrog <simonmrog@gmail.com>
2019-01-16 20:30:38 -0500

    mountain slope calculations

--- mountain-slope.py
+++ mountain-slope.py
@@ -20,7 +20,8 @@ def c (r1, y1, r2, y2):
     """ Calculates the slope with parameters: r1, y1, r2, y2 """
     delta_r = abs (float (r1) - float (r2)) * 1000 #delta_r in metres
     delta_y = abs (float (y1) - float (y2)) #delta_y is already in metres
-    m_percent = delta_y / math.sqrt (delta_r ** 2 - delta_y ** 2) * 100
+    delta_x = math.sqrt (delta_r ** 2 - delta_y ** 2)
+    m_percent = delta_y / delta_x * 100
     m_degrees = math.atan (m_percent / 100) * 180 / math.pi
     click.echo ("Slope: %.2f%%" % m_percent)
     click.echo ("Angle: %.2f°" % m_degrees)
