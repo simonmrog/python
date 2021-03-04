90dd0f1e50df3e4a49d0cb4ecec594b0d76cc11e
simonmrog <simonmrog@gmail.com>
2021-03-04 14:00:57 -0500

    celery example using redis for message and django framework

---
+++ django_celery_redis/django_celery_redis/celery.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_celery_redis.settings')
+
+app = Celery('django_celery_redis')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
