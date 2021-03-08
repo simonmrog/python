6459f1082898db33249af7adb91469af8c9f3909
simonmrog <simonmrog@gmail.com>
2021-03-07 19:55:41 -0500

    basic example using celery with fastapi and rabbitmq

---
+++ fastapi-celery-rabbitmq/first-example/app/core/celery.py
@@ -0,0 +1,15 @@
+from celery import Celery
+
+# CELERY_BROKER = "redis://localhost:6379"
+# CELERY_BACKEND = "redis://localhost:6379"
+
+CELERY_BROKER = "amqp://rabbit:5672"
+CELERY_BACKEND = "rpc://"
+
+
+celery_config = {
+    "broker_url": CELERY_BROKER,
+    "result_backend": CELERY_BACKEND
+}
+
+celery_app = Celery("worker", config_source=celery_config)
