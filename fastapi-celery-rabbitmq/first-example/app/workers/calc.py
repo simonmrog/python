e8bf4ff1d6739949276856ec16669ad794756191
simonmrog <simonmrog@gmail.com>
2021-03-08 16:08:42 -0500

    modifying docker compose

--- fastapi-celery-rabbitmq/first-example/app/workers/calc.py
+++ fastapi-celery-rabbitmq/first-example/app/workers/calc.py
@@ -1,5 +1,6 @@
 from app.core.celery import celery_app
 from celery.utils.log import get_task_logger
+from time import sleep
 
 logger = get_task_logger(__name__)
 
@@ -7,4 +8,5 @@ logger = get_task_logger(__name__)
 @celery_app.task
 def multiply_numbers(a, b):
     logger.info("multiply task running")
+    sleep(10)
     return a * b
