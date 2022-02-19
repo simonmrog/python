4738c286c552daeb8523808797c20bc8e3f4cead
Simón Murillo Gómez <simonmrog@Simons-MacBook-Air.local>
2022-02-19 17:30:33 -0500

    iterator for fibonacci series

--- iterator-examples.py
+++ iterator-examples.py
@@ -26,9 +26,9 @@ class Fibonacci:
 
 
 if __name__ == "__main__":
-    n = int(input())
+    n = 10
     fibonacci_series = Fibonacci(n)
     fibonacci_iterator = iter(fibonacci_series)
 
-    for k in fibonacci_iterator:
-        print(k)
+    fibonacci_array = [k for k in fibonacci_series]
+    print(fibonacci_array)
